3c30d6a855022944c0100b1026966472a2a7c472
hekki <heikkz@gmail.com>
2020-05-17 16:24:29 +0300

    - Activating new account via email

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -2,7 +2,9 @@ package com.example.springredditclone.service;
 
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.model.User;
+import com.example.springredditclone.model.VerificationToken;
 import com.example.springredditclone.repository.UserRepository;
+import com.example.springredditclone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
+
+import static com.example.springredditclone.util.Constants.ACTIVATION_EMAIL;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +23,8 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailContentBuilder mailContentBuilder;
 
     @Transactional
     public void signup(RegisterRequest request) {
@@ -29,6 +36,18 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+        String token = generateVerifyToken(user);
+        String message = mailContentBuilder.build("Thank you for signing up to Spring Reddit, please click on the below url to activate your account : "
+                + ACTIVATION_EMAIL + "/" + token);
+    }
+
+    private String generateVerifyToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 
     private String encodePassword(String password) {
---
+++ src/main/java/com/example/springredditclone/util/Constants.java
@@ -0,0 +1,9 @@
+package com.example.springredditclone.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
+}
